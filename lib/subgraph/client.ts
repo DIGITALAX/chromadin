a29df980efbddd2bf28c1fa0fc0ebb76d8d541cb
emmajane1313 <admin@digitalax.xyz>
2023-04-14 06:31:03 -0400

    fix graph cors error

--- lib/subgraph/client.ts
+++ lib/subgraph/client.ts
@@ -2,31 +2,13 @@ import {
   ApolloClient,
   InMemoryCache,
   HttpLink,
-  ApolloLink,
 } from "@apollo/client";
-import createProxyMiddleware from "http-proxy-middleware";
 
 const httpLink = new HttpLink({
   uri: "https://api.thegraph.com/subgraphs/name/digitalax/chromadin",
 });
 
-const authLink = new ApolloLink((operation, forward) => {
-  const apiKey = process.env.GRAPH_API_KEY;
-
-  operation.setContext({
-    headers: {
-      authorization: `Bearer ${apiKey}`,
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, content-type, Authorization",
-    },
-  });
-
-  return forward(operation);
-});
-
 export const graphClient = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink,
   cache: new InMemoryCache(),
 });
