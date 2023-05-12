2a8533e024704e0b03313e30716240c2bb741d3d
emmajane1313 <admin@digitalax.xyz>
2023-05-12 00:01:42 -0400

    deploy wavs

--- lib/helpers/descriptionRegex.ts
+++ lib/helpers/descriptionRegex.ts
@@ -1,49 +1,69 @@
 const descriptionRegex = (description: string, messages?: boolean) => {
-  const styledText = description?.split(" ")?.map((word: string) => {
-    if (word[0] === "#") {
-      if (messages) {
-        return `<em id="hashtags" style="color: #ff494a; font-style: normal;">${word}</em>`;
-      } else {
-        return `<em id="hashtags" style="color: #81A8F8; font-style: normal;">${word}</em>`;
-      }
-    } else if (word[0] === "@") {
-      if (messages) {
-        return `
-          <span style="color: #ff494a;">${word}</span>
-        
+  const lines = description?.split(/[\r\n]+/);
+  const styledLines = lines?.map((line: string) => {
+    const words = line.split(" ");
+    const styledWords = words.map((word: string) => {
+      if (word[0] === "#") {
+        if (messages) {
+          return `<em id="hashtags" style="color: #ff494a; cursor: pointer; font-style: normal;">${word}</em>`;
+        } else {
+          return `<em id="hashtags" style="color: #81A8F8; cursor: pointer; font-style: normal;">${word}</em>`;
+        }
+      } else if (word[0] === "@") {
+        if (messages) {
+          return `
+            <a href="${`https://lenster.xyz/u/${
+              word?.replace("@", "")?.split(".lens")[0]
+            }`}" rel="noreferrer" target="_blank" style="margin-right: 4px;">
+              <span style="color: #ff494a;">${word}</span>
+            </a>
           `;
-      } else {
-        return `
-          <span style="color: #81A8F8;">${word}</span>
+        } else {
+          return `
+            <a href="${`https://lenster.xyz/u/${
+              word?.replace("@", "")?.split(".lens")[0]
+            }`}" target="_blank" rel="noreferrer" style="margin-right: 4px;">
+              <span style="color: #81A8F8;">${word?.split(".lens")[0]}</span>
+            </a>
           `;
-      }
-    } else if (
-      (word[0] && word[1] && word[2] === "w" && word[3] === ".") ||
-      (word[word.length] === "z" &&
-        word[word.length - 1] === "y" &&
-        word[word.length - 2] === "x" &&
-        word[word.length - 3] === ".") ||
-      (word[word.length] === "m" &&
-        word[word.length - 1] === "o" &&
-        word[word.length - 2] === "c" &&
-        word[word.length - 3] === ".")
-    )
-      if (messages) {
-        return `
-        <span style="color: #ff494a;">${word}</span>
-        `;
+        }
+      } else if (
+        (word[0] && word[1] && word[2] === "w" && word[3] === ".") ||
+        (word[word.length] === "z" &&
+          word[word.length - 1] === "y" &&
+          word[word.length - 2] === "x" &&
+          word[word.length - 3] === ".") ||
+        (word[word.length] === "m" &&
+          word[word.length - 1] === "o" &&
+          word[word.length - 2] === "c" &&
+          word[word.length - 3] === ".")
+      ) {
+        if (messages) {
+          return `
+            <a href=${
+              word?.includes("//") ? word : `//${word}`
+            } target="_blank" rel="noreferrer">
+              <span style="color: #ff494a;">${word}</span>
+            </a>
+          `;
+        } else {
+          return `
+            <a href=${
+              word?.includes("//") ? word : `//${word}`
+            } target="_blank" rel="noreferrer">
+              <span style="color: #81A8F8;">${word}</span>
+            </a>
+          `;
+        }
       } else {
-        return `
-        <span style="color: #81A8F8;">${word}</span>
-        `;
+        return word;
       }
-    else {
-      return word;
-    }
+    });
+    const styledLine = `<span>${styledWords?.join(" ")}</span>`;
+    return styledLine;
   });
-
-  const wrappedText = `<div>${styledText?.join(" ")}</div>`;
-  return wrappedText;
+  const formattedDescription = styledLines?.join("<br /><br />");
+  return `<div>${formattedDescription}</div>`;
 };
 
 export default descriptionRegex;
