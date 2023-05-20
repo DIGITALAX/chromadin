cfa860ba58d88b0ea0ddc1943ca4f2496ab2f6a3
emmajane1313 <admin@digitalax.xyz>
2023-05-20 17:00:58 -0400

    name update

--- components/Home/modules/Vending.tsx
+++ components/Home/modules/Vending.tsx
@@ -323,7 +323,9 @@ const Vending: FunctionComponent<VendingProps> = ({
                       </div>
                       <div className="relative flex flex-row w-full h-fit gap-2  text-sm font-arcade">
                         <div className="relative uppercase text-white w-fit h-fit cursor-pointer whitespace-nowrap">
-                          {collection?.name}
+                          {collection?.name.length > 20
+                            ? collection?.name.slice(0, 20)
+                            : collection?.name}
                         </div>
                         <div className="relative w-full h-fit text-ama justify-end flex">
                           {Number(collection?.tokenIds?.length) -
